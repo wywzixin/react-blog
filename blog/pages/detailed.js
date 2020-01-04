a3f1e8b230c1858c5d2057e5c5b49819e66103d8
wangyawei <1796609990@qq.com>
2020-01-04 10:06:56 +0800

    initialcommit

---
+++ blog/pages/detailed.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Head from 'next/head'
+import Header from '../components/Header.js'
+import { Row, Col } from 'antd'
+
+const Detailed = () => (
+  <div>
+    <Head>
+       <title>Home</title>
+       <link rel="icon" href="/favicon.ico" />
+     </Head>
+     <Header/>
+     <Row className="comm-main" type="flex" justify="center">
+          <Col className="comm-left" xs={24} sm={24} md={16} lg={18} xl={14}>
+            左侧
+          </Col>
+          <Col className="comm-right" xs={0} sm={0} md={7} lg={5} xl={4}>
+            左侧
+          </Col>
+     </Row>
+  </div>
+)
+
+export default Detailed
